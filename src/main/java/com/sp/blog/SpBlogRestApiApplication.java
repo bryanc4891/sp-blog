86654e3050dd0b954e7156a5e47739ab3b831432
bryanc4891 <43765473+bryanc4891@users.noreply.github.com>
2024-06-16 19:06:23 -0700

    initial setup

---
+++ src/main/java/com/sp/blog/SpBlogRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sp.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpBlogRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpBlogRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sp/blog/SpBlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpBlogRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
