3a9f1f4986cdb1578f33f79988335c014e890248
bryanc4891 <43765473+bryanc4891@users.noreply.github.com>
2024-06-19 15:32:50 -0700

    Security/add role and user repository

---
+++ src/main/java/com/sp/blog/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.sp.blog.repository;
+
+import com.sp.blog.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
