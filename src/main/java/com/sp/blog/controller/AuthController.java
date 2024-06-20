64bf2d99b7d15cc854bb8669e4bc05fa5424da43
bryanc4891 <43765473+bryanc4891@users.noreply.github.com>
2024-06-19 17:48:45 -0700

    add signin signup endpoint

---
+++ src/main/java/com/sp/blog/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.sp.blog.controller;
+
+import com.sp.blog.payload.LoginDto;
+import com.sp.blog.payload.RegisterDto;
+import com.sp.blog.service.AuthService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@Slf4j
+public class AuthController {
+
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping(value = {"/login", "/signin"})
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        String response = authService.login(loginDto);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping(value = {"/register", "/signup"})
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+        log.info("Received user data: name={}, username={}, email={}",
+                registerDto.getName(), registerDto.getUsername(), registerDto.getEmail());
+        String response = authService.register(registerDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
